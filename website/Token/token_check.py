6b210d518ce0f7586d80d080ed9d7239de024032
trojanX12388 <roberandrewb.up@gmail.com>
2025-01-27 09:51:02 +0800

    UPDATED TOKEN CHECKING

--- website/Token/token_check.py
+++ website/Token/token_check.py
@@ -18,7 +18,6 @@ from sqlalchemy import update
 
 
 # CHECK TOKEN EXPIRATION
-
 def Check_Token(func):
     @wraps(func)
     def decorated(*args, **kwargs):
@@ -28,14 +27,18 @@ def Check_Token(func):
             user_token = MSLoginToken.query.filter_by(MSId=current_user.MSId).first()
         elif current_user.__class__.__name__ == "FISAdmin":
             user_token = MSLoginToken.query.filter_by(AdminId=current_user.AdminId).first()
-        elif not current_user.__class__.__name__ == "MSAccount":
-            # Handle other user types here if needed
-            flash('Unknown user type.', category='error')
-            return redirect(url_for('auth.Logout'))
         else:
             # Handle other user types here if needed
             flash('Unknown user type.', category='error')
-            return redirect(url_for('admin.adminLogout'))
+            return redirect(url_for('auth.Logout'))
+
+        # Check if user_token exists
+        if not user_token:
+            flash('Account has been logged out. Please log in again.', category='error')
+            if current_user.__class__.__name__ == "MSAccount":
+                return redirect(url_for('auth.Logout'))
+            else:
+                return redirect(url_for('admin.adminLogout'))
 
         try:
             decoded = jwt.decode(user_token.access_token, app.config['SECRET_KEY'], algorithms=['HS256'])
@@ -136,99 +139,3 @@ def refresh_token(user_token):
         else:
             return redirect(url_for('admin.adminLogout'))
         
-
-# # ---------------------------------------------------------
-
-# # SYSTEM ADMIN CHECK TOKEN EXPIRATION
-
-# def SysCheck_Token(func):
-#     @wraps(func)
-#     def decorated(*args, **kwargs):
-#         session['previous_url'] = request.url
-        
-#         if current_user.__class__.__name__ == "FISSystemAdmin":
-#             user_token = FISSystemAdmin.query.filter_by(SystemAdminId=current_user.SystemAdminId).first()
-#         else:
-#             # Handle other user types here if needed
-#             flash('Unknown user type.', category='error')
-#             return redirect(url_for('views.home'))
-
-#         try:
-#             decoded = jwt.decode(user_token.access_token, app.config['SECRET_KEY'], algorithms=['HS256'])
-#             expiration_time = datetime.utcfromtimestamp(decoded['exp'])
-            
-#             if datetime.utcnow() <= expiration_time:
-#                 return func(*args, **kwargs)
-#             else:
-#                 Sysrefresh_token(user_token)
-#                 previous_url = session.pop('previous_url', None)
-#                 if current_user.__class__.__name__ == "FISSystemAdmin":
-#                     return redirect(previous_url or url_for('sysadmin.sysadminH'))
-#                 else:
-#                     return redirect(previous_url or url_for('sysadmin.sysadminL'))
-        
-#         except jwt.ExpiredSignatureError:
-#             try:
-#                 decoded_refresh = jwt.decode(user_token.refresh_token, app.config['REFRESH_TOKEN_SECRET'], algorithms=['HS256'])
-#                 refresh_expiration_time = datetime.utcfromtimestamp(decoded_refresh['exp'])
-                
-#                 if datetime.utcnow() >= refresh_expiration_time:
-#                     flash('Session Expired. Please Login again.', category='error')
-#                     if current_user.__class__.__name__ == "FISSystemAdmin":
-#                         return redirect(url_for('views.home'))
-                    
-#                 else:
-#                     Sysrefresh_token(user_token)
-#                     previous_url = session.pop('previous_url', None)
-#                     if current_user.__class__.__name__ == "FISSystemAdmin":
-#                         return redirect(previous_url or url_for('sysadmin.sysadminH'))
-            
-#             except jwt.ExpiredSignatureError:
-#                 flash('Session Expired. Please Login again.', category='error')
-#                 if current_user.__class__.__name__ == "FISSystemAdmin":
-#                     return redirect(url_for('views.home'))
-            
-#             except jwt.InvalidTokenError:
-#                 flash('Invalid Token. Please Login again.', category='error')
-#                 if current_user.__class__.__name__ == "FISSystemAdmin":
-#                     return redirect(url_for('views.home'))
-        
-#         except Exception as e:
-#             print(f'Error: {str(e)}')
-#             if current_user.__class__.__name__ == "FISSystemAdmin":
-#                 return redirect(url_for('views.home'))
-    
-#     return decorated
-
-
-# # REFRESH TOKEN
-# def Sysrefresh_token(user_token):
-#     refresh_token = user_token.refresh_token
-#     try:
-#         decoded_refresh_token = jwt.decode(refresh_token, app.config['REFRESH_TOKEN_SECRET'], algorithms=['HS256'])
-#         user_id = decoded_refresh_token['user_id']
-
-#         new_access_token = generate_access_token(user_id)
-        
-#         if current_user.__class__.__name__ == "FISSystemAdmin":
-#             u = update(FISSystemAdmin)
-#             u = u.values({"access_token": new_access_token})
-#             u = u.where(FISSystemAdmin.SystemAdminId == current_user.SystemAdminId)
-#             db.session.execute(u)
-#         else:
-#             # Handle other user types here if needed
-#             flash('Unknown user type.', category='error')
-#             if current_user.__class__.__name__ == "FISSystemAdmin":
-#                 return redirect(url_for('views.home'))
-        
-#         db.session.commit()
-#         db.session.close()
-        
-#     except jwt.ExpiredSignatureError:
-#         if current_user.__class__.__name__ == "FISSystemAdmin":
-#             return redirect(url_for('views.home'))
-    
-#     except jwt.InvalidTokenError:
-#         flash('Invalid User Token. Please Login again.', category='error')
-#         if current_user.__class__.__name__ == "FISSystemAdmin":
-#             return redirect(url_for('views.home'))
